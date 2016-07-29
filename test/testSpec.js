020a75bc0ff087f955d862770167525020e5ca44
Nic Higuera <damagecpl@gmail.com>
2016-07-29 14:25:22 -0700

    fixed things

--- test/testSpec.js
+++ test/testSpec.js
@@ -1,1 +1,10 @@
-asdf
+const expect = require('chai').expect
+
+describe("Addition", () => {
+	it('it will make sure the addition function works', () => {
+		expect(1+1).to.equal(2)
+	});
+	it('will make sure 1 + 1 isnt 3', () => {
+		expect(1+1).to.not.equal(3)
+	})
+})
